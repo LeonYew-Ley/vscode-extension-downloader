8a9d78c73803971103378849b068b57f6eefe00c
heshuai <dreamsoul23@qq.com>
2025-12-18 23:06:40 +0800

    fix: 使用正确的unocss集成方式

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginBabel } from '@rsbuild/plugin-babel';
 import { pluginSolid } from '@rsbuild/plugin-solid';
-import { UnoCSSRspackPlugin } from '@unocss/webpack/rspack';
-import { presetWind3 } from '@unocss/preset-wind3';
-
 export default defineConfig({
   html: {
     title: 'VSCode插件下载',
@@ -13,14 +10,5 @@ export default defineConfig({
       include: /\.(?:jsx|tsx)$/,
     }),
     pluginSolid(),
-  ],
-  tools: {
-    rspack: {
-      plugins: [
-        UnoCSSRspackPlugin({
-          presets: [presetWind3()],
-        }),
-      ],
-    },
-  },
+  ]
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import { render } from 'solid-js/web';
-import 'uno.css';
 import './main.css';
 import App from './App';
 
